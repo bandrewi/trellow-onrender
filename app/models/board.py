87a406a7bbf9c5477f1a45ca299e2747c0346630
andrew bui <bandrewi98@gmail.com>
2022-11-15 18:53:21 -0800

    schema defined in each model file

--- app/models/board.py
+++ app/models/board.py
@@ -5,6 +5,9 @@ from sqlalchemy.sql import func
 class Board(db.Model):
     __tablename__ = 'boards'
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+        
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
--- app/models/card.py
+++ app/models/card.py
@@ -5,6 +5,9 @@ from sqlalchemy.sql import func
 class Card(db.Model):
     __tablename__ = 'cards'
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+        
     id = db.Column(db.Integer, primary_key=True)
     order = db.Column(db.Integer, nullable=False)
     title = db.Column(db.String(255), nullable=False)
